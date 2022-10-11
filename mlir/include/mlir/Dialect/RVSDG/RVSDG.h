62042b250868aa561728ad51fb9f717c4fc20dd0
Halvor Bjørstad <43417915+Riphiphip@users.noreply.github.com>
2022-10-11 14:13:36 +0200

    Add first draft of gamma node definition

---
+++ mlir/include/mlir/Dialect/RVSDG/RVSDG.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "mlir/IR/OperationSupport.h"
+#include "mlir/IR/Region.h"
+#include "mlir/IR/OpDefinition.h"
+
+#define GET_OP_CLASSES
+#include "mlir/Dialect/RVSDG/RVSDG.h.inc"
---
+++ mlir/lib/Dialect/RVSDG/IR/RVSDGOps.cpp
@@ -0,0 +1,54 @@
+#include "mlir/IR/OpImplementation.h"
+
+#include "mlir/Dialect/RVSDG/RVSDG.h"
+#include <stdio.h>
+
+using namespace mlir;
+using namespace rvsdg;
+
+/**
+ * @brief Prints out a comma separated list of parameters paired with their
+ * respective types. Having types as a parameter is redundant, but tablegen
+ *won't build without it.
+ *
+ * @param p Assembly printer
+ * @param op Operation which we are printing
+ * @param operands Range of operands to be printed
+ * @param types Types of the operands will be matched with operands using
+ *              position in the array
+ **/
+void printTypedParamList(OpAsmPrinter &p, Operation *op, OperandRange operands,
+                         TypeRange types) {
+
+  int param_count = std::min(operands.size(), types.size());
+  for (int i = 0; i < param_count; ++i) {
+    if (i != 0) {
+      p << ", ";
+    }
+    p.printType(types[i]);
+    p << " ";
+    p.printOperand(operands[i]);
+  }
+}
+
+ParseResult
+parseTypedParamList(OpAsmParser *parser,
+                    SmallVectorImpl<OpAsmParser::OperandType> &operands,
+                    SmallVectorImpl<Type> types) {
+
+  auto parseTypedParam = [&]() -> ParseResult {
+    Type result;
+    if (parser->parseType(result).succeeded()) {
+      return ParseResult::failure();
+    };
+    OpAsmParser::OperandType operand_res;
+    if (parser->parseOperand(operand_res)) {
+      return ParseResult::failure();
+    }
+    return ParseResult::success();
+  };
+  
+  parser->parseCommaSeparatedList(OpAsmParser::Delimiter::Paren,
+                                  parseTypedParam);
+  return ParseResult::success();
+}
